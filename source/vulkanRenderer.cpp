abb04ca273ec2e401e486b9053d5740dea42c044
Nikkel <n.heesen@tu-bs.de>
2019-01-02 23:09:34 +0100

    small bug fix

--- source/vulkanRenderer.cpp
+++ source/vulkanRenderer.cpp
@@ -425,11 +425,11 @@ void vulkan::Renderer::Create2D()
 		attachments[0].flags = 0;
 		attachments[0].format = pSwapchain->settings.surfaceFormat.format;
 		attachments[0].samples = settings.sampleCount;
-		attachments[0].loadOp = VK_ATTACHMENT_LOAD_OP_LOAD; // Load because we do want to render on top for an interface
+		attachments[0].loadOp = (isCreated3D) ? VK_ATTACHMENT_LOAD_OP_LOAD : VK_ATTACHMENT_LOAD_OP_CLEAR; // Load because we do want to render on top for an interface
 		attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
 		attachments[0].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
 		attachments[0].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-		attachments[0].initialLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+		attachments[0].initialLayout = (isCreated3D) ? VK_IMAGE_LAYOUT_PRESENT_SRC_KHR : VK_IMAGE_LAYOUT_UNDEFINED;
 		attachments[0].finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
 		// Reference to the color attachment
